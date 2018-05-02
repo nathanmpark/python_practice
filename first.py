436ce40e60bef30c66ee4f0c8501999889f2e0bc
Nathan Park <nathan@objectlabs.com>
2018-05-02 16:11:13 -0700

    initial practice

---
+++ first.py
@@ -0,0 +1,13 @@
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+print("number of fruits: " + str(fruits.count('apple')))
+
+print("count of tangerine: " + str(fruits.count('tangerine')))
+  # fruits.index('banana')
+  # fruits.index('banana', 4)  # Find next banana starting a position 4
+  # fruits.reverse()
+  # ['banana', 'apple', 'kiwi', 'banana', 'pear', 'apple', 'orange']
+  # fruits.append('grape')
+  # fruits
+  # fruits.sort()
+  # fruits
+  # fruits.pop()
